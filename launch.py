f5a4fae477e7622874e5951f3c641f48751af5ec
steven nikolaev <stilver8@gmail.com>
2019-11-06 21:34:22 +0500

    added settings

--- launch.py
+++ launch.py
@@ -1,5 +1,18 @@
 import pygame
+from settings.color import WHITE
+from settings.settings_reader import SETTINGS
 
+pygame.init()
+clock = pygame.time.Clock()
 
-if __name__ == "__main__":
-    pygame.init()
+main_surf = pygame.display.set_mode(SETTINGS.get('RESOLUTION'))
+
+pygame.draw.rect(main_surf, WHITE, (50, 50, 80, 30))
+
+pygame.display.update()
+
+while True:
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            exit()
+    clock.tick(SETTINGS.get('FPS'))
